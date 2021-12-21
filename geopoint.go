44a64e0907fe36eb3032d44cab5f5afa623ceef4
Alexander Trakhimenok <alex@trakhimenok.com>
2021-12-21 01:01:24 +0000

    Added JSON tags to GeoPoint struct

--- geopoint.go
+++ geopoint.go
@@ -1,11 +1,16 @@
 package geopoint
 
 // Formula to calculate distance between two GeoPoints.
-type Formula func(one, two *GeoPoint) Kilometres
+type Formula func(one, two GeoPoint) Kilometres
 
-// GeoPoint represents GPS coordinates.
+// GeoPoint represents GPS coordinates. Has no filtered or unexported fields.
 type GeoPoint struct {
-	Latitude, Longitude Degrees
+
+	// The latitude in degrees. It must be in the range [-90.0, +90.0].
+	Latitude Degrees `json:"latitude,omitempty"`
+
+	// The longitude in degrees. It must be in the range [-180.0, +180.0].
+	Longitude Degrees `json:"longitude,omitempty"`
 }
 
 // NewGeoPoint returns a pointer to a new GeoPoint.
@@ -17,6 +22,6 @@ func NewGeoPoint(latitude, longitude Degrees) *GeoPoint {
 }
 
 // DistanceTo calculates distance to another GeoPoint.
-func (g *GeoPoint) DistanceTo(another *GeoPoint, f Formula) Kilometres {
+func (g GeoPoint) DistanceTo(another GeoPoint, f Formula) Kilometres {
 	return f(g, another)
 }
